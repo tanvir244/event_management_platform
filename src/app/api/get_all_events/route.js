914eed23673ffebee09f09850b9b78bfa8e219be
tanvir244 <tr30365s25@gmail.com>
2024-09-10 12:45:55 +0600

    deploy

--- src/app/api/get_all_events/route.js
+++ src/app/api/get_all_events/route.js
@@ -6,9 +6,8 @@ export const GET = async (response) => {
         const db = await connectDB();
         const allEventsCollection = db.collection('allEvents');
         const allEvents = await allEventsCollection.find().toArray();
-        // return data 
-        return new NextResponse(JSON.stringify(allEvents), {status: 200});
+        return new NextResponse(JSON.stringify(allEvents));
     } catch (error) {
-        return NextResponse.json({message: 'something went wrong'}, {status: 500});
+        return new NextResponse(JSON.stringify({message: 'Server error'}, {status: 500}))
     }
 }
