c14fc3efb135f6e88ec65285e154ef17785ed1cc
tanvir244 <tr30365s25@gmail.com>
2024-09-10 16:09:32 +0600

    deploy

--- src/app/api/get_all_events/route.js
+++ src/app/api/get_all_events/route.js
@@ -1,13 +1,13 @@
 import { connectDB } from "@/lib/connectDB"
 import { NextResponse } from "next/server";
 
-export const GET = async (response) => {
+export const GET = async () => {
     try {
         const db = await connectDB();
         const allEventsCollection = db.collection('allEvents');
         const allEvents = await allEventsCollection.find().toArray();
-        return new NextResponse(JSON.stringify(allEvents));
+        return new NextResponse(JSON.stringify(allEvents), { status: 200 });
     } catch (error) {
-        return new NextResponse(JSON.stringify({message: 'Server error'}, {status: 500}))
+        return new NextResponse(JSON.stringify({ message: 'Server error' }), { status: 500 });
     }
 }
