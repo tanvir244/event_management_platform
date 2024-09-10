819f721198c873751bc2a547541d22d81cd0456c
tanvir244 <tr30365s25@gmail.com>
2024-09-10 18:31:53 +0600

    deploy

--- src/app/api/get_location/[location]/route.js
+++ src/app/api/get_location/[location]/route.js
@@ -5,7 +5,7 @@ export const GET = async (response, {params}) => {
     try {
         const db = await connectDB();
         const allEventsCollection = db.collection('allEvents');
-        const location = await allEventsCollection.find({category: params.location}).toArray();
+        const location = await allEventsCollection.find({location: params.location}).toArray();
         return new NextResponse(JSON.stringify(location));
     } catch (error) {
         return new NextResponse(JSON.stringify({message: 'Server error'}, {status: 500}))
